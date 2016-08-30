29fdd296c7d98015044ef3ba9d52f648a9348084
Frédéric Letellier <frederic.letellier.pro@gmail.com>
2016-08-30 15:16:02 +0200

    Modify the GCE starter code to pull jokes from Java library

--- backend/src/main/java/com/joke/endpoint/backend/MyEndpoint.java
+++ backend/src/main/java/com/joke/endpoint/backend/MyEndpoint.java
@@ -9,8 +9,7 @@ package com.joke.endpoint.backend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
-
-import javax.inject.Named;
+import com.joke.JokeProvider;
 
 /** An endpoint class we are exposing */
 @Api(
@@ -24,11 +23,11 @@ import javax.inject.Named;
 )
 public class MyEndpoint {
 
-    /** A simple endpoint method that takes a name and says Hi back */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    /** A simple endpoint method to provide a joke */
+    @ApiMethod(name = "getJoke")
+    public MyBean getJoke(){
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+        response.setData(JokeProvider.getJoke());
 
         return response;
     }
