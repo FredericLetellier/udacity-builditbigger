677bbf0412be0231c87182aa24d6d388d8597b42
Frédéric Letellier <frederic.letellier.pro@gmail.com>
2016-08-30 16:41:51 +0200

    Connect Android app to the backend (real device with local dev server)

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -6,8 +6,6 @@ import android.os.AsyncTask;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
-import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.joke.JokeActivity;
 import com.joke.endpoint.backend.myApi.MyApi;
 
@@ -31,15 +29,9 @@ public class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
             MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
                     new AndroidJsonFactory(), null)
                     // options for running against local devappserver
-                    // - 10.0.2.2 is localhost's IP address in Android emulator
-                    // - turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                        @Override
-                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                            abstractGoogleClientRequest.setDisableGZipContent(true);
-                        }
-                    });
+                    // - set to test on a physical device with a local development server
+                    // (to get your ip address, go to cmd -> ipconfig -> IPV4 address with port number)
+                    .setRootUrl("http://192.168.43.170:8080/_ah/api/");
             // end options for devappserver
 
             myApiService = builder.build();
